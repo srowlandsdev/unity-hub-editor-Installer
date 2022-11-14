3da4cfea7e445ee2a2aab003823986acbcd72351
Sam Price <sampricegd@hotmail.co.uk>
2022-11-14 11:49:57 +0000

    [COMMIT] Adding WIP multi installer
    At the request of a new feature, I am adding a WIP version of a multiple-version installer file

---
+++ unity_multi_editor_cli_installer.bat
@@ -0,0 +1,68 @@
+@echo off
+set CURL_DOWNLOAD_PATH=%1
+set UNITY_VERSIONS=%2
+set UNITY_PATH="C:\Program Files\Unity\Hub\Editor\%UNITY_VERSION%\Editor\Unity.exe"
+set HUB_INSTALLED_PATH="C:\Program Files\Unity Hub\Unity Hub.exe"
+
+:VarChecker
+if "%1" == "" (
+	echo CURL_DOWNLOAD_PATH is empty
+	goto VarErr
+)
+if "%2" == "" (
+	echo UNITY_VERSIONS is empty
+	goto VarErr
+)
+
+
+:GetUnityHub
+echo Downloading Unity Hub to: %CURL_DOWNLOAD_PATH%
+echo.-----------------------------------------------------------------------------------------------------------------------------.
+
+if exist %CURL_DOWNLOAD_PATH%\UnityHubSetup.exe (
+	echo UnityHubSetup.exe already downloaded to %CURL_DOWNLOAD_PATH%, moving to installation
+	goto InstallEditorVersion
+) else (
+	curl -o "%CURL_DOWNLOAD_PATH%"/UnityHubSetup.exe https://public-cdn.cloud.unity3d.com/hub/prod/UnityHubSetup.exe
+)
+
+:InstallUnityHub
+call "%CURL_DOWNLOAD_PATH%\UnityHubSetup.exe" /S
+
+(for %%a in(%UNITY_VERSIONS%) do (
+	goto InstallEditorVersion
+)
+
+:InstallEditorVersion
+if exist %UNITY_PATH% (
+	echo Unity version %UNITY_VERSION% already installed at %UNITY_PATH%, moving to module installation
+	goto InstallEditorModules
+) else (
+	echo Installing Unity Editor version: %UNITY_VERSION%
+	echo Release versions available:
+	call %HUB_INSTALLED_PATH% -- --headless editors -r
+	echo.-----------------------------------------------------------------------------------------------------------------------------.
+	echo Installed editor versions:
+	call %HUB_INSTALLED_PATH% -- --headless editors -i
+	echo.-----------------------------------------------------------------------------------------------------------------------------.
+	echo Current install path for editor version:
+	call %HUB_INSTALLED_PATH% -- --headless ip -g
+	echo.-----------------------------------------------------------------------------------------------------------------------------.
+	echo Running install command:
+	call %HUB_INSTALLED_PATH% -- --headless install --version %UNITY_VERSION%
+	echo.-----------------------------------------------------------------------------------------------------------------------------.
+)
+
+:InstallEditorModules
+echo Installing editor modules
+call %HUB_INSTALLED_PATH% -- --headless install-modules --version %UNITY_VERSION% -m ios android --childModules
+echo.-----------------------------------------------------------------------------------------------------------------------------.
+goto Complete
+
+:VarErr
+echo Error encountered during installation due to missing or improper variable
+exit 1
+
+:Complete
+echo Unity installation has now completed for version %UNITY_VERSION%
+exit 0
